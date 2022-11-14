f2d8e819bb3cf1e096b7729190afb484e4beed7f
bra-fsn <nagy.attila@gmail.com>
2022-11-14 11:45:43 +0100

    Add eu-central-2

--- cloudperf/providers/aws.py
+++ cloudperf/providers/aws.py
@@ -30,6 +30,7 @@ region_map = {
     "EU (Milan)": "eu-south-1",
     "EU (Paris)": "eu-west-3",
     "EU (Stockholm)": "eu-north-1",
+    "Europe (Zurich)": "eu-central-2",
     "Middle East (Bahrain)": "me-south-1",
     "Middle East (UAE)": "me-central-1",
     "South America (Sao Paulo)": "sa-east-1",
